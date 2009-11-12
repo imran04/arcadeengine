f369dfca6bc9e40cdf27fb2282e6ccfc7cd4fd1e
ziliak@hotmail.com <ziliak@hotmail.com@6df6b432-ba52-11dd-a720-8bda5143708b>
2009-11-12 15:49:12 +0000

    [Examples] Text demo

---
+++ trunk/Examples/Text Demo/Program.cs
@@ -0,0 +1,127 @@
+﻿#region Licence
+//
+//This file is part of ArcEngine.
+//Copyright (C)2008-2009 Adrien Hémery ( iliak@mimicprod.net )
+//
+//ArcEngine is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//any later version.
+//
+//ArcEngine is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+#endregion
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ArcEngine;
+using ArcEngine.Asset;
+using ArcEngine.Graphic;
+using ArcEngine.Input;
+
+
+namespace ArcEngine.Examples.TextDemo
+{
+	/// <summary>
+	/// Main game class
+	/// </summary>
+	public class EmptyProject : Game
+	{
+
+		/// <summary>
+		/// Main entry point.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			try
+			{
+				using (EmptyProject game = new EmptyProject())
+					game.Run();
+			}
+			catch (Exception e)
+			{
+				// Oops, an error happened !
+				MessageBox.Show(e.StackTrace, e.Message);
+			}
+		}
+
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public EmptyProject()
+		{
+			CreateGameWindow(new Size(1024, 768));
+		}
+
+
+
+		/// <summary>
+		/// Load contents 
+		/// </summary>
+		public override void LoadContent()
+		{
+			Display.ClearColor = Color.CornflowerBlue;
+
+			Font = Font2d.CreateFromTTF(@"c:\windows\fonts\verdana.ttf", 14, FontStyle.Regular);
+
+		}
+
+
+		/// <summary>
+		/// Unload contents
+		/// </summary>
+		public override void UnloadContent()
+		{
+			if (Font != null)
+			{
+				Font.Dispose();
+				Font = null;
+			}
+		}
+
+
+
+
+		/// <summary>
+		/// Called when it is time to draw a frame.
+		/// </summary>
+		/// <param name="device"></param>
+		public override void Draw()
+		{
+			// Clears the background
+			Display.ClearBuffers();
+
+			Font.DrawText(new Point(100, 50), "zeiodsiuposdiufosidufosiu");
+
+		}
+
+
+
+
+		#region Properties
+
+
+		/// <summary>
+		/// Font
+		/// </summary>
+		Font2d Font;
+
+
+		/// <summary>
+		/// Tileset
+		/// </summary>
+		TileSet Tileset;
+
+		#endregion
+
+	}
+
+}
