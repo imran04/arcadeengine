617f91b8a0890c6bc3e765849805f574e521ab13
ziliak@hotmail.com <ziliak@hotmail.com@6df6b432-ba52-11dd-a720-8bda5143708b>
2009-11-17 08:04:52 +0000

    [Game] GameComponent

---
+++ trunk/Framework/GameComponent.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArcEngine
+{
+	/// <summary>
+	/// Provides a modular way of adding functionality to a game.
+	/// </summary>
+	public class GameComponent
+	{
+		public GameComponent()
+		{
+			IsVisible = true;
+			Enabled = true;
+
+
+		}
+
+		/// <summary>
+		/// Called when the GameComponent needs to be initialized. 
+		/// </summary>
+		public virtual void Initialize()
+		{
+		}
+
+
+		/// <summary>
+		/// Called when the GameComponent needs to be updated. 
+		/// </summary>
+		/// <param name="time">Time elapsed since the last call to Update</param>
+		public virtual void Update(GameTime time)
+		{
+		}
+
+
+		/// <summary>
+		/// Called when the DrawableGameComponent needs to be drawn. 
+		/// </summary>
+		public virtual void Draw()
+		{
+		}
+
+
+		/// <summary>
+		/// Indicates the order in which the GameComponent should be updated relative to other GameComponent instances. 
+		/// Lower values are updated first. 
+		/// </summary>
+		public int UpdateOrder
+		{
+			get;
+			protected set;
+		}
+
+
+		/// <summary>
+		/// Order in which the component should be drawn.
+		/// Lower values are updated first. 
+		/// </summary>
+		public int DrawOrder
+		{
+			get;
+			protected set;
+		}
+
+		/// <summary>
+		/// Indicates whether should be called when Game.Update is called.
+		/// </summary>
+		public bool Enabled
+		{
+			get;
+			protected set;
+		}
+
+
+		/// <summary>
+		/// Indicates whether Draw should be called.
+		/// </summary>
+		public bool IsVisible
+		{
+			get;
+			protected set;
+		}
+	}
+}
---
+++ trunk/Framework/GameComponentCollection.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ArcEngine
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class GameComponentCollection : Collection<GameComponent>
+	{
+
+		#region overrides
+
+		/// <summary>
+		/// Removes all children from the collection. 
+		/// </summary>
+		protected override void ClearItems()
+		{
+			for (int i = 0; i < base.Count; i++)
+			{
+				OnComponentRemoved(base[i]);
+			}
+			base.ClearItems();
+		}
+
+
+		/// <summary>
+		/// Inserts a <see cref="IGameComponent"/> into the collection at the specified location. 
+		/// </summary>
+		/// <param name="index">Position</param>
+		/// <param name="item">Component</param>
+		protected override void InsertItem(int index, GameComponent item)
+		{
+			if (base.IndexOf(item) != -1)
+			{
+				return;
+			}
+
+			base.InsertItem(index, item);
+			if (OnComponentAdded != null)
+			{
+				OnComponentAdded(item);
+			}
+		}
+
+
+		/// <summary>
+		/// Removes a <see cref="IGameComponent"/> object in the collection. 
+		/// </summary>
+		/// <param name="index">Position</param>
+		protected override void RemoveItem(int index)
+		{
+			GameComponent gameComponent = base[index];
+			base.RemoveItem(index);
+			if (gameComponent != null)
+			{
+				OnComponentRemoved(gameComponent);
+			}
+		}
+
+
+		/// <summary>
+		/// Modifies the specified <see cref="IGameComponent"/> object in the collection. 
+		/// </summary>
+		/// <param name="index">Position</param>
+		/// <param name="item">Component</param>
+		protected override void SetItem(int index, GameComponent item)
+		{
+			//base.SetItem(index, item);
+		}
+
+
+		#endregion
+
+
+		#region Events
+
+		/// <summary>
+		/// Arguments used with events from the GameComponentCollection. 
+		/// </summary>
+		/// <param name="component">Added component</param>
+		public delegate void ComponentAddedEventHandler(GameComponent component);
+
+	
+		/// <summary>
+		/// Raised when a component is added to the GameComponentCollection.
+		/// </summary>
+		public event ComponentAddedEventHandler OnComponentAdded;
+
+
+
+		/// <summary>
+		/// Arguments used with events from the GameComponentCollection. 
+		/// </summary>
+		/// <param name="component">Removed component</param>
+		public delegate void ComponentRemovedEventHandler(GameComponent component);
+
+
+		/// <summary>
+		/// 	 Raised when a component is removed from the GameComponentCollection. 
+		/// </summary>
+		public event ComponentRemovedEventHandler OnComponentRemoved;
+
+		#endregion
+	}
+}
