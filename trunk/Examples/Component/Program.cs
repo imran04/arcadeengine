df5faf0f307c10a960e62cdbc3bb5c796ce1af5e
ziliak@hotmail.com <ziliak@hotmail.com@6df6b432-ba52-11dd-a720-8bda5143708b>
2009-11-17 08:08:21 +0000

    GameComponent example

---
+++ trunk/Examples/Component/Program.cs
@@ -0,0 +1,128 @@
+﻿#region Licence
+//
+//This file is part of ArcEngine.
+//Copyright (C)2008-2009 Adrien Hémery ( iliak@mimicprod.net )
+//
+//ArcEngine is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//any later version.
+//
+//ArcEngine is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+#endregion
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ArcEngine;
+using ArcEngine.Asset;
+using ArcEngine.Graphic;
+using ArcEngine.Input;
+
+
+namespace ArcEngine.Examples.Component
+{
+	/// <summary>
+	/// A simple GameComponent example
+	/// </summary>
+	public class Example : Game
+	{
+
+		/// <summary>
+		/// Main entry point.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			try
+			{
+				using (Example game = new Example())
+					game.Run();
+			}
+			catch (Exception e)
+			{
+				// Oops, an error happened !
+				MessageBox.Show(e.StackTrace, e.Message);
+			}
+		}
+
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public Example()
+		{
+			CreateGameWindow(new Size(1024, 768));
+
+			// New component
+			ScreenFader fader = new ScreenFader();
+
+			// Setup component
+			fader.FadeColor = Color.CornflowerBlue;
+			fader.Speed = TimeSpan.FromSeconds(5);
+
+			Components.Add(fader);
+		}
+
+
+
+		/// <summary>
+		/// Load contents 
+		/// </summary>
+		public override void LoadContent()
+		{
+			Display.ClearColor = Color.Black;
+
+
+		}
+
+
+		/// <summary>
+		/// Unload contents
+		/// </summary>
+		public override void UnloadContent()
+		{
+		}
+
+
+		/// <summary>
+		/// Update the game logic
+		/// </summary>
+		/// <param name="gameTime"></param>
+		public override void Update(GameTime gameTime)
+		{
+			// Check if the Escape key is pressed
+			if (Keyboard.IsKeyPress(Keys.Escape))
+				Exit();
+
+
+		}
+
+
+
+		/// <summary>
+		/// Called when it is time to draw a frame.
+		/// </summary>
+		public override void Draw()
+		{
+			// Clears the background
+			Display.ClearBuffers();
+
+		}
+
+
+
+
+		#region Properties
+
+		#endregion
+
+	}
+
+}
