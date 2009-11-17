4c672d410e54c8b0cb58d266175ffd7906dcc9e7
ziliak@hotmail.com <ziliak@hotmail.com@6df6b432-ba52-11dd-a720-8bda5143708b>
2009-11-17 08:08:21 +0000

    GameComponent example

---
+++ Examples/Component/ScreenFader.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ArcEngine;
+using ArcEngine.Graphic;
+using System.Drawing;
+
+
+namespace ArcEngine.Examples.Component
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ScreenFader : GameComponent
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public override void Initialize()
+		{
+			
+		}
+
+
+		/// <summary>
+		/// Update every frame
+		/// </summary>
+		/// <param name="time">Elapsed game time</param>
+		public override void Update(GameTime time)
+		{
+			// nothing to update
+			if (CurrentStep > Speed)
+				return;
+
+			// Update current timer
+			CurrentStep += time.ElapsedGameTime;
+		}
+
+
+		/// <summary>
+		/// Draws the component
+		/// </summary>
+		public override void Draw()
+		{
+			// Compute the alpha value
+			int alpha = (int) ((CurrentStep.TotalMilliseconds / Speed.TotalMilliseconds) * 255);
+
+			// Get the fade color
+			Color color = Color.FromArgb(alpha, FadeColor);
+
+			// Fade the full screen
+			Display.FillRectangle(Display.ViewPort, color);
+		}
+
+
+
+		/// <summary>
+		/// Starts the effect
+		/// </summary>
+		public void Start()
+		{
+			CurrentStep = TimeSpan.Zero;
+		}
+
+
+
+		/// <summary>
+		/// Stops the effect
+		/// </summary>
+		public void Stop()
+		{
+			CurrentStep = TimeSpan.MaxValue;
+		}
+
+
+
+		#region Properties
+
+		/// <summary>
+		/// Fade speed
+		/// </summary>
+		public TimeSpan Speed;
+
+
+		/// <summary>
+		/// Current step
+		/// </summary>
+		TimeSpan CurrentStep;
+
+
+		/// <summary>
+		/// Fading color
+		/// </summary>
+		public Color FadeColor;
+
+		#endregion
+
+	}
+}
