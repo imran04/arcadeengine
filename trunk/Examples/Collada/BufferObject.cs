8000a9e216b3bf92500d990de0040d7d444864ff
ziliak@hotmail.com <ziliak@hotmail.com@6df6b432-ba52-11dd-a720-8bda5143708b>
2010-03-24 14:31:04 +0000

    New COLLADA example

---
+++ trunk/Examples/Collada/BufferObject.cs
@@ -0,0 +1,142 @@
+﻿#region Licence
+//
+//This file is part of ArcEngine.
+//Copyright (C)2008-2009 Adrien Hémery ( iliak@mimicprod.net )
+//
+//ArcEngine is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//any later version.
+//
+//ArcEngine is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenTK.Graphics.OpenGL;
+using ArcEngine.Graphic;
+using System.Runtime.InteropServices;
+
+
+namespace ArcEngine.Examples
+{
+	/// <summary>
+	/// Stores user datas in graphic card's memory
+	/// </summary>
+	public class BufferObject<T> : IDisposable where T : struct
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public BufferObject()
+		{
+			GL.GenBuffers(1, out Handle);
+
+			Buffer = new List<T>();
+			ElementSize = Marshal.SizeOf(typeof(T));
+		}
+
+
+
+		/// <summary>
+		/// Dispose
+		/// </summary>
+		public void Dispose()
+		{
+			if (Handle != 0)
+			{
+				GL.DeleteBuffers(1, ref Handle);
+				Handle = 0;
+			}
+
+		}
+
+
+
+		/// <summary>
+		/// Clears the buffer
+		/// </summary>
+		public void Clear()
+		{
+			Buffer.Clear();
+		}
+
+
+		/// <summary>
+		/// Adds a value to the buffer
+		/// </summary>
+		/// <param name="value">Value to add</param>
+		public void Add(T value)
+		{
+			Buffer.Add(value);
+		}
+
+
+
+		/// <summary>
+		/// Apply updates
+		/// </summary>
+		public void Apply()
+		{
+			if (!Display.Capabilities.HasVBO)
+				return;
+
+			try
+			{
+				GL.BindBuffer(BufferTarget.ArrayBuffer, Handle);
+				GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(Count * ElementSize), Buffer.ToArray(), BufferUsageHint.StaticDraw);
+			}
+			catch (Exception e)
+			{
+				bool er = GL.IsBuffer(Handle);
+				Trace.WriteLine(e.Message + Environment.NewLine + e.StackTrace);
+			}
+
+
+		}
+
+
+
+
+		#region Properties
+
+		/// <summary>
+		/// Buffer internal handle
+		/// </summary>
+		int Handle;
+
+
+		/// <summary>
+		/// Internal buffer
+		/// </summary>
+		List<T> Buffer;
+
+
+		/// <summary>
+		/// Number of elements in the buffer
+		/// </summary>
+		public int Count
+		{
+			get
+			{
+				return Buffer.Count;
+			}
+		}
+
+
+		/// <summary>
+		/// Size of an element
+		/// </summary>
+		int ElementSize;
+
+
+		#endregion
+	}
+}
---
+++ trunk/Examples/Collada/Program.cs
@@ -0,0 +1,132 @@
+﻿#region Licence
+//
+//This file is part of ArcEngine.
+//Copyright (C)2008-2009 Adrien Hémery ( iliak@mimicprod.net )
+//
+//ArcEngine is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//any later version.
+//
+//ArcEngine is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using ArcEngine.Asset;
+using ArcEngine.Graphic;
+using ArcEngine.Input;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+// http://www.wazim.com/Collada_Tutorial_1.htm
+// http://www.wazim.com/Collada_Tutorial_2.htm
+namespace ArcEngine.Examples
+{
+	/// <summary>
+	/// Main game class
+	/// </summary>
+	public class Collada : GameBase
+	{
+
+		/// <summary>
+		/// Main entry point.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			try
+			{
+				using (Collada game = new Collada())
+					game.Run();
+			}
+			catch (Exception e)
+			{
+				// Oops, an error happened !
+				MessageBox.Show(e.StackTrace, e.Message);
+			}
+		}
+
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public Collada()
+		{
+			CreateGameWindow(new Size(1024, 768));
+			Window.Text = "Collada loader";
+		}
+
+
+
+		/// <summary>
+		/// Load contents 
+		/// </summary>
+		public override void LoadContent()
+		{
+			Display.ClearColor = Color.CornflowerBlue;
+
+			Mesh = new Mesh();
+			Mesh.Load("data/cube.dae");
+		}
+
+
+		/// <summary>
+		/// Unload contents
+		/// </summary>
+		public override void UnloadContent()
+		{
+			Mesh.Dispose();
+		}
+
+
+		/// <summary>
+		/// Update the game logic
+		/// </summary>
+		/// <param name="gameTime"></param>
+		public override void Update(GameTime gameTime)
+		{
+			// Check if the Escape key is pressed
+			if (Keyboard.IsKeyPress(Keys.Escape))
+				Exit();
+
+
+		}
+
+
+
+		/// <summary>
+		/// Called when it is time to draw a frame.
+		/// </summary>
+		public override void Draw()
+		{
+			// Clears the background
+			Display.ClearBuffers();
+
+
+
+		}
+
+
+
+
+		#region Properties
+
+
+		Mesh Mesh;
+
+
+		#endregion
+
+	}
+
+}
