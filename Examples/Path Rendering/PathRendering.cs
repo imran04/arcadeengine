144fabe80958d504218ad6cc13351469b5707b60
ziliak@hotmail.com <ziliak@hotmail.com@6df6b432-ba52-11dd-a720-8bda5143708b>
2011-09-08 06:38:35 +0000

    Path Rendering demo

---
+++ Examples/Path Rendering/PathRendering.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace ArcEngine.Examples.PathRenderingDemo
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	class PathRendering : IDisposable
+	{
+
+		public PathRendering()
+		{
+
+		}
+
+
+		public void Dispose()
+		{
+		}
+
+
+		#region Properties
+
+		#endregion
+
+	}
+}
